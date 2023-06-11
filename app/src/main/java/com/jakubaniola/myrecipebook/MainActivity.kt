a7c4586bb445062e783b7be98937a9ed17878839
Jakub Anioła <bards95@gmail.com>
2023-06-11 11:24:03 +0200

    Gradle, Search, Topbar, Components module

--- app/src/main/java/com/jakubaniola/myrecipebook/MainActivity.kt
+++ app/src/main/java/com/jakubaniola/myrecipebook/MainActivity.kt
@@ -3,21 +3,17 @@ package com.jakubaniola.myrecipebook
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Surface
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.material3.ExperimentalMaterial3Api
+import com.jakubaniola.myrecipebook.ui.mainlist.RecipesListScreen
 import com.jakubaniola.myrecipebook.ui.theme.MyRecipeBookTheme
 
+@ExperimentalMaterial3Api
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
             MyRecipeBookTheme {
-
+                RecipesListScreen()
             }
         }
     }
--- app/src/main/java/com/jakubaniola/myrecipebook/ui/mainlist/RecipesListScreen.kt
+++ app/src/main/java/com/jakubaniola/myrecipebook/ui/mainlist/RecipesListScreen.kt
@@ -1,1 +1,67 @@
 package com.jakubaniola.myrecipebook.ui.mainlist
+
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.rounded.Add
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.FloatingActionButton
+import androidx.compose.material3.Icon
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.tooling.preview.Preview
+import com.jakubaniola.components.RecipeSearchBar
+import com.jakubaniola.components.TopBar
+import com.jakubaniola.myrecipebook.R
+import com.jakubaniola.myrecipebook.ui.theme.MyRecipeBookTheme
+
+@ExperimentalMaterial3Api
+@Composable
+fun RecipesListScreen(
+    modifier: Modifier = Modifier
+) {
+    Scaffold(
+        topBar = {
+            TopBar(stringResource(id = R.string.app_name))
+        },
+        floatingActionButton = {
+            FloatingActionButton(
+                shape = MaterialTheme.shapes.extraLarge,
+                onClick = {}
+            ) {
+                Icon(
+                    imageVector = Icons.Rounded.Add,
+                    contentDescription = ""
+                )
+            }
+        },
+        content = { paddingValues ->
+            Column(
+                modifier = modifier
+                    .padding(
+                        top = paddingValues.calculateTopPadding()
+                    )
+            ) {
+                RecipeSearchBar {
+                }
+                Text(
+                    text = "Future list",
+
+                )
+            }
+        },
+    )
+}
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Preview
+@Composable
+fun RecipesListScreenPreview() {
+    MyRecipeBookTheme {
+        RecipesListScreen()
+    }
+}
